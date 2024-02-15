cafa757d87068f17b9e646664262b9d9bd8fe1db
HumbertoChiesi <66853228+HumbertoChiesi@users.noreply.github.com>
2024-02-14 23:37:07 -0300

    remove back folder

---
+++ main.py
@@ -0,0 +1,10 @@
+from pokebayimagedownloader.CardsImageDownloader import CardsImageDownloader
+
+
+def main():
+    aux = CardsImageDownloader()
+    aux.download_by_collection('ex8')
+
+
+if __name__ == '__main__':
+    main()
---
+++ pokebayimagedownloader/__init__.py
@@ -0,0 +1,1 @@
+from .CardsImageDownloader import CardsImageDownloader
